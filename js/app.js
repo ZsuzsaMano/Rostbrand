46804bfb2c945749384f825c288baeaae80d617d
Mano <manoaway@gmail.com>
2020-08-08 22:28:55 +0200

    remove Vue, add pages

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const app = new Vue({
-  el: '#app',
-  data: {
-    nav: ['about', 'prints', 'paintings', 'sculptures', 'design', 'contact'],
-    links: '#about',
-    type: ['image', 'statue'],
-    properties: ['title', 'size', 'material', 'price'],
-  },
-});
--- js/components/description.js
+++
@@ -1,4 +0,0 @@
-const Description = Vue.component('description', {
- template: '<div class="description"><h2>{{properties[0]}}</h2>
-  </div>'
-});
