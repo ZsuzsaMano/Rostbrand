268c22b4d845b3a9f7ecdb5a0a05c4f82449eadd
Mano <manoaway@gmail.com>
2020-09-02 12:34:37 +0200

    add css class to print

--- js/handlebar.js
+++ js/handlebar.js
@@ -3,7 +3,6 @@
 const appSource   = document.getElementById('gallery-template').innerHTML;
 const appTemplate = Handlebars.compile(appSource);
 const appContext = { galleryName: [
-
   { name: 'From Ashes Rise (100 x 150 cm, Acrylic on canvas)', link: './img/season5/From Ashes Rise  (100 x 150 cm, Acrylic on canvas).jpg' },
   { name: 'From Her Lips to Gods Ears (40 x 60 cm, Acrylic on canvas)', link: './img/season5/From Her Lips to Gods Ears (40 x 60 cm, Acrylic on canvas).jpg' },
   { name: 'His Hero Is Gone (80  x 100 cm , Acrylic and Aching on iron plate)', link: './img/season5/His Hero Is Gone (80  x 100 cm , Acrylic and Aching on iron plate).jpg' },
